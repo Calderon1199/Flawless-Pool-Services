a456071141cc6699576d590a0d8672fa28e789cb
Eric Truong <truong.eric916@gmail.com>
2024-03-07 21:46:07 -0800

    poosay

--- flawlesspools/src/app/page.js
+++ flawlesspools/src/app/page.js
@@ -8,7 +8,6 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.js</code>
         </p>
-        <h1 className="text-blue-300 border border-red-500"> UR GAY</h1>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
